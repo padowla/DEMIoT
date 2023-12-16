1542a596037a94471826cae99a42926ebf86aa8c
padowla <62257411+padowla@users.noreply.github.com>
2023-12-17 00:25:19 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-
-# Elimina tutti i container Docker in esecuzione
-docker rm -f $(docker ps -aq) 2>/dev/null
-
-# Elimina tutte le immagini Docker
-docker rmi -f $(docker images -aq) 2>/dev/null
-
-# Elimina la cache Docker (volumi, reti, cache)
-docker system prune -af --volumes
-
-# Esegui il comando Docker Compose (aggiungi qui il tuo comando docker-compose up ...)
+echo "[+] Starting DEMIoT..."
 docker compose up -d --build --force-recreate
-
+echo "[+] Succesfully deployed. Enjoy :)"
